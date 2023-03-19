a52bbe7f62815c0c1f81dd89fd6b7a4a567dc4a4
kyeongho <dagh1218@naver.com>
2023-03-19 17:20:59 +0900

    #7 api 요청 보낼 떄 한글의 경우 정확하지 않은 데이터 추출 문제 수정
    - 한글의 경우 UTF-8을 명시하지 않으면 올바른 데이터가 요청되지 않아 다른 블로그 검색 값들이 출력되는 문제 수정
    - api 요청 시 encoding 타입을 UTF-8로 지정

--- simple-blog-finder/src/main/java/com/guardjo/simpleblogfinder/service/BlogSearchService.java
+++ simple-blog-finder/src/main/java/com/guardjo/simpleblogfinder/service/BlogSearchService.java
@@ -3,7 +3,10 @@ package com.guardjo.simpleblogfinder.service;
 import com.guardjo.simpleblogfinder.constant.BlogSearchConstant;
 import com.guardjo.simpleblogfinder.dto.KakaoBlogSearchRequest;
 import com.guardjo.simpleblogfinder.dto.KakaoBlogSearchResponse;
+import io.netty.util.CharsetUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriBuilder;
@@ -28,6 +31,7 @@ public class BlogSearchService {
                 .queryParam("sort", request.getSort())
                 .queryParam("page", request.getPage())
                 .queryParam("size", request.getSize())
+                .encode(CharsetUtil.UTF_8)
                 .build()
                 .toUri();
 
