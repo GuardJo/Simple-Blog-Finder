80a3f8c41b39a5edb1c46e3ce09f74d743dff3fe
kyeongho <dagh1218@naver.com>
2023-03-22 22:46:50 +0900

    #15 openapi를 활용하여 api 명세 작성
    - openapi 어노테이션 활용하여 명세 작성

--- simple-blog-finder/src/main/java/com/guardjo/simpleblogfinder/controller/BlogSearchController.java
+++ simple-blog-finder/src/main/java/com/guardjo/simpleblogfinder/controller/BlogSearchController.java
@@ -3,21 +3,32 @@ package com.guardjo.simpleblogfinder.controller;
 import com.guardjo.simpleblogfinder.constant.BlogSearchConstant;
 import com.guardjo.simpleblogfinder.dto.BlogSearchRequest;
 import com.guardjo.simpleblogfinder.dto.BlogSearchResponse;
-import com.guardjo.simpleblogfinder.dto.kakao.KakaoBlogSearchRequest;
 import com.guardjo.simpleblogfinder.dto.SearchTermDto;
+import com.guardjo.simpleblogfinder.dto.kakao.KakaoBlogSearchRequest;
+import com.guardjo.simpleblogfinder.dto.kakao.KakaoBlogSearchResponse;
 import com.guardjo.simpleblogfinder.service.BlogSearchService;
 import com.guardjo.simpleblogfinder.service.SearchManagementService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
+@Tag(name = "블로그 검색 관련 API", description = "검색어를 기반으로한 검색정보 반환 및 인기 검색어 조회가 가능하다.")
 @RestController
 @RequestMapping(BlogSearchConstant.REST_URL_PREFIX)
 @Slf4j
@@ -31,6 +42,20 @@ public class BlogSearchController {
         this.searchManagementService = searchManagementService;
     }
 
+    @Operation(summary = "검색 결과 반환", description = "검색어를 입력하여 검색 정보 반환")
+    @Parameters(value = {
+            @Parameter(name = "searchValue", description = "검색어"),
+            @Parameter(name = "searchApiType", description = "검색 사이트 종류"),
+            @Parameter(name = "sort", description = "정렬 종류 : accuracy(정확도순), recency(최신순)"),
+            @Parameter(name = "size", description = "반환 받는 한 페이지의 크기"),
+            @Parameter(name = "page", description = "반환 받는 목록의 페이지 index")
+    })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "카카오 블로그 검색 조회 결과",
+            content = @Content(schema = @Schema(implementation = KakaoBlogSearchResponse.class))),
+            @ApiResponse(responseCode = "400", description = "검색 요청 데이터 오류",
+                    content = @Content(schema = @Schema(implementation = HttpServletResponse.class)))
+    })
     @Cacheable
     @GetMapping(BlogSearchConstant.REQUEST_BLOG_SEARCH_URL)
     public BlogSearchResponse searchBlog(@RequestParam(defaultValue = BlogSearchConstant.KAKAO_API) String searchApiType,
@@ -56,6 +81,11 @@ public class BlogSearchController {
         return response;
     }
 
+    @Operation(summary = "인기 검색어 조회", description = "그간 검색된 검색어 중 상위 10위권 내 검색어 목록 반환")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "인기 검색어 목록 반환",
+            content = @Content(array = @ArraySchema(schema = @Schema(implementation = SearchTermDto.class))))
+    })
     @GetMapping(BlogSearchConstant.REQUEST_BLOG_SEARCH_KEYWORD_TOP_TEN)
     public List<SearchTermDto> findSearchTermRanking() {
         log.info("[Test] Request SearchTerm TOP10 Ranking");
